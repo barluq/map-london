93474627c37b9df89b995c1ba6f58a182337d264
Lukas Bares <47695733+barluq@users.noreply.github.com>
2024-12-11 00:30:25 +0100

    render basic map

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { ref, onMounted } from 'vue'
+
+
+onMounted(() => {
+  const map = L.map('map').setView([51.505, -0.09], 13); // Set map center and zoom level
+
+  // Add a tile layer (this example uses OpenStreetMap)
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <aside>
+    <div id="map"></div>
+  </aside>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#map {
+  height: 100vh;
+  width: 100vw;
 }
 </style>
